8afe1b8939d231385631de9b04974669feb24c32
SageSG <houliangyip@gmail.com>
2022-10-24 19:14:04 +0800

    fix cart

--- frontend/src/components/admin/CoursesCatalog.jsx
+++ frontend/src/components/admin/CoursesCatalog.jsx
@@ -2,12 +2,14 @@ import React, {useEffect, useState} from 'react'
 import {AddCourseModal} from '../../components/admin/AddCourseModal'
 import courseService from '../../services/courses';
 import {ConfirmationModal} from '../../modals/ConfirmationModal'
+import {AiOutlineEdit, AiOutlineDelete} from 'react-icons/ai'
 
 const CoursesCatalog = () => {
   const [dataCourses, setDataCourses] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [confirmDeleteModal, setConfirmDeleteModal] = useState(false);
   const [courseInfo, setCourseInfo] = useState({});
+  const [sortTable, setSortTable] = useState("_id");
 
   useEffect(() => {
     retrieveCourses();
@@ -42,12 +44,34 @@ const CoursesCatalog = () => {
     setConfirmDeleteModal(true);
   }
 
+  function sortObjectArrayByProperty(array, property) {
+    array.sort(function(a, b) {
+      if (a[property] < b[property]) {
+        return -1;
+      } else if (a[property] > b[property]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  const sortListing = (e) => {
+    setSortTable(e.target.value)
+  }
+
   
   return (
     <div className="flex flex-row flex-wrap w-full bg-w1 rounded m-8 p-8 content-start overflow-y-auto">
-      <span className="flex w-full font-type1 text-[20px] font-bold">
+      <div className="flex w-full font-type1 text-[20px] font-bold">
         Courses Catalog
-      </span>
+        <div className='text-sm ml-4 self-center'>Sort By: 
+          <select className='border-2 border-black ml-2' onChange={sortListing}>
+          <option value="_id">Id</option>
+          <option value="courseName">Name</option>
+          <option value="courseType">Type</option>
+          <option value="quantity">Quantity</option>
+          </select></div>
+      </div>
       <div className="flex justify-between w-full mb-4">
       <p className="flex mt-2 mb-2">Add, update, deactivate courses</p>
       <button className="align-end bg-success text-w1 px-4 font-bold font-type1 rounded-sm" onClick={()=>openEditorModal(null)}>Add new courses</button>
@@ -57,21 +81,24 @@ const CoursesCatalog = () => {
         <table className="table w-full table-striped table-bordered table-hover">
           <thead>
             <tr>
-              <th className='w-1/5 border'>ID</th>
-              <th className='w-1/5 border'>Name</th>
-              <th className='w-1/5 border'>Img</th>
-              <th className='w-1/5 border'>Quantity</th>
-              <th className='w-1/5 border'>Actions</th>
+              <th className='w-1/6 border'>ID</th>
+              <th className='w-1/6 border'>Name</th>
+              <th className='w-1/6 border'>Type</th>
+              <th className='w-1/6 border'>Img</th>
+              <th className='w-1/6 border'>Quantity</th>
+              <th className='w-1/6 border'>Actions</th>
             </tr>
           </thead>
           <tbody className="table-group-divider">
-            {dataCourses.map((course) => (
-              <tr key={course.id}>
+            {dataCourses.sort(sortObjectArrayByProperty(dataCourses, sortTable)).map((course) => (
+              <tr key={course._id}>
                 <td className='w-1/6'>{course._id.substring(course._id.length-5)}</td>
                 <td className='w-1/6'><b>{course.company }</b><br/> {  course.courseName}</td>
                 <td className='w-1/6'>
+                  {course.courseType}
+                </td>
+                <td className='w-1/6'>
                 <img
-                    // src={`/api/image/${images.getImg(course.courseImg)}`}
                     src={`http://localhost:5000/v1/api/images/getImg/${course.courseImg}`}
                     className="flex h-[120px] w-full object-stretch"
                     alt={"exmaple"}
@@ -79,18 +106,25 @@ const CoursesCatalog = () => {
                 <td className='w-1/6'>{course.quantity}</td>
               <td>
                 <div className="flex flex-wrap w-full space-y-2">
-                <button className="flex bg-yellow-400 text-w1 px-4 w-full font-bold font-type1 rounded-lg" onClick={()=>{openEditorModal(course)}}>Edit</button>
-                <button className="flex bg-danger text-w1 px-4 w-full font-bold font-type1 rounded-lg" onClick={()=>{deleteCourseModal(course)}}>Delete</button>
+                <button className="flex bg-yellow-400 w-full h-[40px] justify-center font-bold font-type1 rounded-lg" onClick={()=>{openEditorModal(course)}}>
+                  <span className='self-center'>
+                  Edit
+                    </span> 
+                  <AiOutlineEdit className='self-center'/></button>
+                <button className="flex bg-danger w-full h-[40px] font-bold justify-center font-type1 rounded-lg" onClick={()=>{deleteCourseModal(course)}}>
+                  <span className='self-center'>Delete</span> 
+                  <AiOutlineDelete className='self-center'/></button>
                 </div>
               </td>
                 </tr>
             ))}
+
             
           </tbody>
         </table>
       </div>
               {confirmDeleteModal && <ConfirmationModal closeModal={setConfirmDeleteModal} courseInfo={courseInfo}/>}
-              {modalIsOpen && <AddCourseModal closeModal={setModalIsOpen} courseInfo={courseInfo}/>}
+              {modalIsOpen && <AddCourseModal closeModal={setModalIsOpen} courseInfo={courseInfo} setCourseInfo={setCourseInfo}/>}
     </div>
   )
 }
--- frontend/src/routes/Cart.jsx
+++ frontend/src/routes/Cart.jsx
@@ -1,64 +1,54 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState, useCallback} from 'react'
 import cartsService from "../services/carts";
-import toast from "react-hot-toast";
+import { CreditCardModal } from '../modals/CreditCardModal'
 import { useDispatch, useSelector } from "react-redux";
 import { getUserDetails, getCartNumber } from "../features/user/userActions";
-import { CreditCardModal } from '../modals/CreditCardModal'
+import toast from 'react-hot-toast';
 
 const Cart = () => {
-  
-  const [cartList, setCartList] = useState([]);
-  const [checkout, setCheckout] = useState([]);
   const [showModal, setShowModal] = useState(false)
   const { userInfo, userId } = useSelector((state) => state.user);
+  const [cartList, setCartList] = useState([])
+  const [checkout, setCheckout] = useState([])
   const dispatch = useDispatch();
 
-  const [checkedState, setCheckedState] = useState();
-  useEffect(() => {
-    if(userInfo) dispatch(getUserDetails());
-    window.scrollTo(0, 0);
-    
-    if(userInfo) {getCart()} else {toast.error('Please login to view cart')};
-
-  }, [userInfo]);
-
-  useEffect(() => {
-    setCheckedState(new Array(cartList.length).fill(false));
-  }, cartList);
-
   // retrieve cart
-  const getCart = () => {
+const getCart = useCallback(() => {
+  if(userInfo){
     cartsService
-      .getCart(userId)
-      .then((response) => {
-        console.log(userId);
-        console.log(response.data);
-        setCartList(response.data.coursesAdded);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
+    .getCart(userId)
+    .then((response) => {
+      setCartList(response.data.coursesAdded);
+      console.log(response.data.coursesAdded)
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  }else{
+    toast.error("Please login to view your cart")
+  }
+  
+},[userInfo, userId]);
 
-  const handleOnChange = (position) => {
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-    );
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    getCart();
+  }, [userInfo, getCart])
 
-    setCheckedState(updatedCheckedState);
+  const [checkedState, setCheckedState] = useState(
+    new Array(cartList.length).fill(false)
+);
 
-    // const totalPrice = updatedCheckedState.reduce(
-    //   (sum, currentState, index) => {
-    //     if (currentState === true) {
-    //       return sum + toppings[index].price;
-    //     }
-    //     return sum;
-    //   },
-    //   0
-    // );
+const handleOnChange = (position) => {
+  const updatedCheckedState = checkedState.map((item, index) =>
+    index === position ? !item : item
+  );
 
-    // setTotal(totalPrice);
-  };
+  setCheckedState(updatedCheckedState);
+
+};
+
+
   
   function checkOutItems(){
     console.log("call stripe api")
@@ -70,49 +60,41 @@ const Cart = () => {
 
   }
 
-  // retrieve cart
-  const deleteCart = (courseId) => {
+  function removeItem(item, position){
     cartsService
-      .deleteCart(userId, courseId)
+      .deleteCart(userId, item.courseId)
       .then((response) => {
         console.log(response.data);
         setCartList(response.data.coursesAdded);
+        const updatedCheckedState = checkedState.map((item, index) =>
+    index === position ? !item : item
+  );
+
+  setCheckedState(updatedCheckedState);
+    if(checkout.includes(item)){
+      setCheckout([...checkout.filter((element)=>{return element!==item})])
+    }
+    
+    setCartList([...cartList.filter((element)=>{return element!==item})])
         toast.success("Items successfully deleted from cart.");
       })
       .catch((e) => {
         console.log(e);
         toast.success("Items failed to deleted from cart.");
       });
-  };
+      dispatch(getCartNumber(userId));
+    
+  }
 
-  function removeItem(item, position) {
-    deleteCart(item.courseId);
-    dispatch(getCartNumber(userId));
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-    );
-
-    setCheckedState(updatedCheckedState);
-    if (checkout.includes(item)) {
-      setCheckout([
-        ...checkout.filter((element) => {
-          return element !== item;
-        }),
-      ]);
+  function addToCheckout(item){
+    if(checkout.includes(item)){
+      setCheckout([...checkout.filter((element)=>{return element!==item})])
+    }else{
+      setCheckout([...checkout, item])
     }
   }
 
-  function addToCheckout(item) {
-    if (checkout.includes(item)) {
-      setCheckout([
-        ...checkout.filter((element) => {
-          return element !== item;
-        }),
-      ]);
-    } else {
-      setCheckout([...checkout, item]);
-    }
-  }
+  
 
   return (
     <main className="flex w-full h-full min-h-[600px] px-[40px] lg:px-[120px] pb-[24px] bg-b1 ">
@@ -121,154 +103,101 @@ const Cart = () => {
           <span className="font-type3 font-medium text-[2vw] text-b3 leading-[32px]">
             Shopping Cart List
           </span>
-
-          <hr className="flex flex-wrap w-full border-1 border-[black] self-center m-2" />
+          
+          <hr className="flex flex-wrap w-full border-1 border-[black] self-center m-2"/>
         </div>
+        
 
         <div className="flex flex-row flex-nowrap w-full">
-          <table className="flex-row w-full table-auto text-center border-separate">
-            <thead>
-              <tr>
-                <th className="border border-b1">Selection</th>
-                <th className="border border-b1">Course</th>
-                <th className="border border-b1">Quantity</th>
-                <th className="border border-b1">U.P Price</th>
-                <th className="border border-b1">Current Price</th>
-                <th className="border border-b1">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {cartList.map((item, index) => (
-                <tr key={index}>
-                  <td>
-                    <input
-                      type="checkbox"
-                      className="w-[20px] h-[20px]"
-                      checked={checkedState[index]}
-                      onChange={() => handleOnChange(index)}
-                      onClick={() => addToCheckout(item)}
-                    />
-                  </td>
-                  <td>{item.courseName}</td>
-                  <td>{item.quantity}</td>
-                  <td>${item.usualPriceTotal}</td>
-                  <td>${item.currentPriceTotal}</td>
-                  <td>
-                    <button
-                      className="btn bg-red-500"
-                      onClick={() => removeItem(item, index)}
-                    >
-                      delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <div></div>
+          <table className='flex-row w-full table-auto text-center border-separate'>
+        <thead>
+    <tr>
+      <th className='border border-b1'>Selection</th>
+      <th className='border border-b1'>Course</th>
+      <th className='border border-b1'>Quantity</th>
+      <th className='border border-b1'>U.P Price</th>
+      <th className='border border-b1'>Current Price</th>
+      <th className='border border-b1'>Actions</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    {cartList.map((item, index) => (
+      <tr key={index}>
+      <td><input type="checkbox" className="w-[20px] h-[20px] mt-4" checked={checkedState[index]}
+  onChange={() => handleOnChange(index)} onClick={()=>addToCheckout(item)}/></td>
+      <td>{item.courseName}</td>
+      <td>{item.quantity}</td>
+      <td>${item.usualPriceTotal}</td>
+      <td>${item.currentPriceTotal}</td>
+      <td><button className='btn bg-red-500' onClick={()=>removeItem(item, index)}>delete</button></td>
+
+    </tr>
+    ))}
+    
+    </tbody>
+  </table>
+          <div>
+          </div>
         </div>
       </div>
       <div className="flex flex-row flex-wrap w-1/3 h-fit mt-[24px]">
-        <div className="flex flex-wrap w-full h-fit bg-white mx-[16px] p-[24px] border-[1px] border-[#E4E5E7] rounded self-start">
-          <div className="flex w-full h-fit border-b-[2px] border-b3 shadow-price-quote">
-            <span className="font-type1 text-[1.4vw] text-b3 font-bold">
-              Shopping Cart{" "}
-            </span>
-          </div>
-          <div className="flex flex-wrap w-full my-4">
+        
+      <div className="flex flex-wrap w-full h-fit bg-white mx-[16px] p-[24px] border-[1px] border-[#E4E5E7] rounded self-start">
+
+        
+
+
+        <div className="flex w-full h-fit border-b-[2px] border-b3 shadow-price-quote">
+        <span className="font-type1 text-[1.4vw] text-b3 font-bold">Shopping Cart </span>
+        </div>
+        <div className="flex flex-wrap w-full my-4">
             {checkout.map((item, index) => (
-              <div
-                key={index}
-                className="flex flex-row justify-between w-full h-fit"
-              >
-                <span className="font-type1 font-bold text-[1vw] text-[#55585D] leading-[22px] self-center">
-                  {item.courseName}
-                </span>
-                <span className="font-type1 text-[1vw] text-b1 font-bold">
-                  ${item.currentPriceTotal}
-                </span>
+              <div key={index} className="flex flex-row justify-between w-full h-fit">
+                <span className="font-type1 font-bold text-[1vw] text-[#55585D] leading-[22px] self-center">{item.courseName} x{item.quantity}</span>
+                <span className="font-type1 text-[1vw] text-b1 font-bold">${item.usualPriceTotal}</span>
               </div>
             ))}
-          </div>
+            </div>
 
-          <div className="flex flex-wrap w-full">
-            <hr className="flex flex-wrap w-full border-1 border-[#55585D] self-center my-2" />
+            <div className="flex flex-wrap w-full">
+            <hr className="flex flex-wrap w-full border-1 border-[#55585D] self-center my-2"/>
+              
+                <div className="flex flex-row justify-between w-full h-fit">
+                <span className="font-type1 font-bold text-[1vw] text-[#55585D] leading-[22px] self-center">Subtotal</span>
+                <span className="font-type1 text-[1vw] text-b1 font-bold">${checkout.reduce((partialSum, a) => partialSum + parseFloat(a.usualPriceTotal), 0.00).toFixed(2)}</span>
+                </div>
+                <div className="flex flex-row justify-between w-full h-fit">
+                <span className="font-type1 font-bold text-[1vw] text-[#55585D] leading-[22px] self-center">Discounts</span>
+                <span className="font-type1 text-[1vw] text-b1 font-bold">- ${checkout.reduce((partialSum, a) => partialSum + parseFloat(a.usualPriceTotal - a.currentPriceTotal), 0.00).toFixed(2)}
+                </span>     
+                </div>
+                <hr className="flex flex-wrap w-full border-1 border-[#55585D] self-center my-2"/>
+                <div className="flex flex-row justify-between w-full h-fit my-2">
+                <span className="font-type1 font-bold text-[1vw] text-black leading-[22px] self-center">Grand Total</span>
+                <span className="font-type1 text-[1vw] text-b1 font-bold">${checkout.reduce((partialSum, a) => partialSum + parseFloat(a.currentPriceTotal), 0.00).toFixed(2)}</span>
+                </div>
+                <div className="flex flex-row justify-between w-full h-fit">
+                <span className="font-type1 font-bold text-[1vw] text-[#55585D] leading-[22px] self-center">Donated ($1 per $10)</span>
+                <span className="font-type1 text-[1vw] text-b1 font-bold">${(checkout.reduce((partialSum, a) => partialSum + parseFloat(a.currentPriceTotal), 0.00)/10).toFixed(0)}</span>
+                </div>
+                
+            </div>
 
-            <div className="flex flex-row justify-between w-full h-fit">
-              <span className="font-type1 font-bold text-[1vw] text-[#55585D] leading-[22px] self-center">
-                Subtotal
-              </span>
-              <span className="font-type1 text-[1vw] text-b1 font-bold">
-                $
-                {checkout
-                  .reduce(
-                    (partialSum, a) => partialSum + parseFloat(a.usualPrice),
-                    0.0
-                  )
-                  .toFixed(2)}
-              </span>
-            </div>
-            <div className="flex flex-row justify-between w-full h-fit">
-              <span className="font-type1 font-bold text-[1vw] text-[#55585D] leading-[22px] self-center">
-                Discounts
-              </span>
-              <span className="font-type1 text-[1vw] text-b1 font-bold">
-                - $
-                {checkout
-                  .reduce(
-                    (partialSum, a) =>
-                      partialSum + parseFloat(a.usualPrice - a.discountedPrice),
-                    0.0
-                  )
-                  .toFixed(2)}
-              </span>
-            </div>
-            <div className="flex flex-row justify-between w-full h-fit">
-              <span className="font-type1 font-bold text-[1vw] text-[#55585D] leading-[22px] self-center">
-                GrandTotal
-              </span>
-              <span className="font-type1 text-[1vw] text-b1 font-bold">
-                $
-                {checkout
-                  .reduce(
-                    (partialSum, a) =>
-                      partialSum + parseFloat(a.discountedPrice),
-                    0.0
-                  )
-                  .toFixed(2)}
-              </span>
-            </div>
-            <hr className="flex flex-wrap w-full border-1 border-[#55585D] self-center my-2" />
-            <div className="flex flex-row justify-between w-full h-fit my-2">
-              <span className="font-type1 font-bold text-[1vw] text-black leading-[22px] self-center">
-                Grand Total
-              </span>
-              <span className="font-type1 text-[1vw] text-b1 font-bold">
-                $
-                {(
-                  checkout.reduce(
-                    (partialSum, a) =>
-                      partialSum + parseFloat(a.discountedPrice),
-                    0.0
-                  ) / 10
-                ).toFixed(0)}
-              </span>
-            </div>
-          </div>
+        <button
+          className="flex w-full h-[40px] rounded-sm bg-green-500 justify-center items-center mt-4"
+          onClick={() => checkOutItems()}
+        >
+          <span className="font-type1 font-bold text-[14px] leading-[22px] text-white">
+            Checkout Items
+          </span>
+        </button>
 
-          <button
-            className="flex w-full h-[40px] rounded-sm bg-red-500 justify-center items-center"
-            onClick={() => checkOutItems()}
-          >
-            <span className="font-type1 font-bold text-[14px] leading-[22px] text-white">
-              Checkout Items
-            </span>
-          </button>
-        </div>
+      </div>
       </div>
       {showModal && <CreditCardModal closeModal={setShowModal}/>}
     </main>
-  );
-};
+  )
+}
 
-export default Cart;
+export default Cart
