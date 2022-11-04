f283531aad86da7bcef7a850c26c5c3a3cd0c9ea
Anne Tan <SIT2002716@gmail.com>
2022-11-05 02:44:49 +0800

    Update SuspiciousActivityList.js

--- frontend/src/components/admin/SuspiciousActivityList.js
+++ frontend/src/components/admin/SuspiciousActivityList.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import SuspiciousActivity from './SuspiciousActivity'
 
-export default function SuspiciousActivityList( {logs} ){    
+export default function SuspiciousActivityList({ logs }) {
     return (
-        logs.map(log =>{
-            return (                
+        logs.map(log => {
+            return (
                 <SuspiciousActivity key={log._id} log={log} />
             )
         })
