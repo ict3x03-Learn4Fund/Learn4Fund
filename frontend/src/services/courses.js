b9c5fc6f241e2f6a76c3491735973cc4eb2e67c9
Anne Tan <SIT2002716@gmail.com>
2022-11-05 03:14:02 +0800

    Update courses.js

--- frontend/src/services/courses.js
+++ frontend/src/services/courses.js
@@ -1,21 +1,21 @@
 import http from "../http-common"
 
-const getAll = async () =>{
+const getAll = async () => {
     const response = await http.get("/courses")
     return response;
 }
 
-const deleteCourse = async (id) =>{
+const deleteCourse = async (id) => {
     const response = await http.post(`/courses/delete/${id}`)
     return response;
 }
 
-const createCourse = async (data) =>{
+const createCourse = async (data) => {
     const response = await http.post("/courses/create", data)
     return response;
 }
 
-const updateCourse = async (id, data) =>{
+const updateCourse = async (id, data) => {
     const response = await http.put(`/courses/update/${id}`, data)
     return response;
 }
@@ -23,7 +23,7 @@ const updateCourse = async (id, data) =>{
 const uploadCourseImage = async (imgId) => {
     const response = await http.post(`/courses/uploadCourseImage`, imgId);
     return response;
-  }
+}
 
 const coursesService = {
     getAll,
