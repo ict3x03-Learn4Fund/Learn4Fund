d43125a213179bb049e8243a6f0c21432925afc5
Yip Hou Liang, Henry <houliangyip@gmail.com>
2022-11-04 15:15:39 +0800

    change the condition for session update time (#160)
    * recaptcha test
    
    * Link courses
    
    * Upload image for courses
    
    * fix login issues
    
    * fix login
    
    * fix login
    
    * OTP fix
    
    * time log out
    
    * fix register otp
    
    * trying to fix google recaptcha
    
    * Fix sign up
    
    * merge changes
    
    * fix payment on change issue
    
    * Update OTPModal.jsx
    
    * only allow users who bought the course make reviews
    
    * fix cart and voucher api
    
    * Update payments.controller.js
    
    * Update package.json
    
    * modal close for cart
    
    * Register page
    
    * Code comment for login + sign up
    
    * Code cleanup
    
    * Code cleanup
    
    * Code cleanup
    
    * Code cleanup
    
    * clean up
    
    * Update http-common.js
    
    * truncate text and banner
    
    * backend session termination
    
    * prices for add courses
    
    * Pushed some changes
    
    * Update http-common.js
    
    * sanitise donation amts and course amts
    
    * Check if ipaddress is valid
    
    * Login modal updated
    
    * add session time
    
    * Update authMiddleware.js
    
    Co-authored-by: SHENGYU-AS-PC\sheng <shengyu98@hotmail.com>

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -29,7 +29,7 @@ const protect = asyncHandler((req,res,next) =>{
                     //get user from token
                     req.account = await Account.findById(decoded.id)                        //[Authentication] Get user from token, and set as req.account
                     
-                    if(url !== '/v1/api/accounts/getAccount'){
+                    if(url !== '/v1/api/accounts' && url !== '/v1/api/carts'){
                     //update user session timestamp
                     await Account.findByIdAndUpdate(decoded.id, {loggedTimestamp: Date.now()}) //[Authentication] Update user session timestamp
                     }
