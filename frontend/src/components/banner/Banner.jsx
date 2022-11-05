d6c885125ed6fa38c68ad07641b4f89d77bbae08
SageSG <houliangyip@gmail.com>
2022-11-06 05:13:05 +0800

    Styling issues

--- frontend/src/components/banner/Banner.jsx
+++ frontend/src/components/banner/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = (props) => {
     return (
         <div className='sticky bottom-0 flex h-[80px] w-full bg-black opacity-80 justify-center' style={{ zIndex: 49 }}>
             <span onClick={() => { Close() }} className="cursor-pointer absolute right-2 top-2"><AiOutlineCloseCircle className='text-w1 text-[24px] self-center' /></span>
-            <div className="cursor-pointer flex flex-col font-type1 font-bold text-[24px] self-center text-w1 w-fit h-fit    " onClick={() => { props.setPromoModal(true) }}>More Discounts 4 Our Members! Find Out More.</div>
+            <div className="cursor-pointer flex flex-col font-type1 font-bold text-[24px] self-center text-w1 w-fit h-fit    " onClick={() => { props.setPromoModal(true) }}>More Discounts For Our Members! Find Out More.</div>
         </div>
     )
 }
